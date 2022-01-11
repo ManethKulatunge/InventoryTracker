66b0eb4368369a6b68dc524ad4d429e6d78c2bc2
Maneth <maneth.kulatunge@mail.mcgill.ca>
2022-01-10 21:08:48 -0500

    put and delete routes added

--- models/inventory.js
+++ models/inventory.js
@@ -30,7 +30,11 @@ const Inventory = mongoose.model('Inventory', new mongoose.Schema({
 
 function validateInventory(inventory) {
   const schema = {
-    name: Joi.string().min(5).required()
+    name: Joi.string().min(5).required(),
+    summary:Joi.string().max(140),
+    price:Joi.number().integer().required(),
+    quantity:Joi.number().integer().required(),
+    category:Joi.array()
   };
 
   return Joi.validate(inventory, schema);
--- routes/inventory.js
+++ routes/inventory.js
@@ -3,22 +3,50 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-
-    let item = new Inventory({ 
-        name: req.body.name,
-      });
-      item = await item.save();
-      
-      res.send(item);
-});
-
 router.get('/', async (req, res) => {
     const customers = await Inventory.find().sort('name');
     res.send(customers);
 });
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    let item = new Inventory({ 
+        name: req.body.name,
+        price:req.body.price, 
+        quantity:req.body.quantity, 
+        summary:req.body.summary, 
+        category:req.body.category
+      });
+      item = await item.save();
+      res.send(item);
+});
+
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
   
+    const item = await Inventory.findByIdAndUpdate(req.params.id,
+      {
+        name: req.body.name,
+        price:req.body.price, 
+        quantity:req.body.quantity, 
+        summary:req.body.summary, 
+        category:req.body.category
+      }, { new: true });
+  
+    if (!item) return res.status(404).send('The item with the given ID was not found.');
+    
+    res.send(item);
+  });
+
+  router.delete('/:id', async (req, res) => {
+    const item = await Inventory.findByIdAndRemove(req.params.id);
+  
+    if (!item) return res.status(404).send('The item with the given ID was not found.');
+  
+    res.send(item);
+  });
+
 
 module.exports = router;
