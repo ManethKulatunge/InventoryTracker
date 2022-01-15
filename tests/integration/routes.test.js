0994ff7bac056c64eb562c49d3c0e490c6c50f4d
Maneth <maneth.kulatunge@mail.mcgill.ca>
2022-01-15 03:26:33 -0500

    PUT requests made

--- tests/integration/routes.test.js
+++ tests/integration/routes.test.js
@@ -24,9 +24,9 @@ afterAll(done => {
 
 describe('POST /api/inventory', () => {
 
-    it("should SAVE to database and RETURN 200 and the product details", async () => {
+    it("should SAVE to database and RETURN 200 and the item details", async () => {
         const res = await request.post('/api/inventory').send({
-            name : "product1",
+            name : "item1",
             price : 55,
             quantity : 33
         })
@@ -36,14 +36,14 @@ describe('POST /api/inventory', () => {
         // Ensures response contains id,name,price,and quantity
         expect(res.body).toHaveProperty('_id', 'name', 'price', 'quantity')
 
-        // Searches the product in the database
-        const product = await Inventory.findOne({ name: 'product1' })
-        expect(product.name).toBeTruthy()
-        expect(product.price).toBeTruthy()
-        expect(product.quantity).toBeTruthy()
+        // Searches the item in the database
+        const item = await Inventory.findOne({ name: 'item1' })
+        expect(item.name).toBeTruthy()
+        expect(item.price).toBeTruthy()
+        expect(item.quantity).toBeTruthy()
     });
 
-    it("should SAVE to database and RETURN 200 and the product details (with category and summary)", async () => {
+    it("should SAVE to database and RETURN 200 and the item details (with category and summary)", async () => {
         const res = await request.post('/api/inventory').send({
             name : "fooditem1",
             price : 55,
@@ -57,11 +57,11 @@ describe('POST /api/inventory', () => {
         // Ensures response contains id,name,price,and quantity
         expect(res.body).toHaveProperty('_id', 'name', 'price', 'quantity', 'category', 'summary')
 
-        // Searches the product in the database
-        const product = await Inventory.findOne({ name: 'product1' })
-        expect(product.name).toBeTruthy()
-        expect(product.price).toBeTruthy()
-        expect(product.quantity).toBeTruthy()
+        // Searches the item in the database
+        const item = await Inventory.findOne({ name: 'item1' })
+        expect(item.name).toBeTruthy()
+        expect(item.price).toBeTruthy()
+        expect(item.quantity).toBeTruthy()
     });
 
     it("should return 400, and error: name should be atleast 5 characters", async () => {
@@ -175,47 +175,47 @@ describe('POST /api/inventory', () => {
 });
 
 describe('GET /api/inventory', () => {
-    it("should return product and 200 status", async () => {
-        //create new product and save it in the database
-        const product = new Inventory({ name: 'stationerybundle',price:45,quantity:33 });
-        await product.save()
+    it("should return item and 200 status", async () => {
+        //create new item and save it in the database
+        const item = new Inventory({ name: 'stationerybundle',price:45,quantity:33 });
+        await item.save()
 
-        //check if result from get request contains aforementioned product
+        //check if result from get request contains aforementioned item
         const res = await request.get("/api/inventory").send()
         expect(res.status).toEqual(200);
         expect(res.body.some(entry => entry.name === 'stationerybundle')).toBeTruthy();
     });
 
-    it("should return all products and 200 status", async () => {
-        //create new products and save it in the database
-        const products = [
+    it("should return all items and 200 status", async () => {
+        //create new items and save it in the database
+        const items = [
             { name: 'stationerybundle',price:45,quantity:33 },
-            { name: 'productbundle',price:55,quantity:33 }
+            { name: 'itembundle',price:55,quantity:33 }
         ]
 
-        await Inventory.collection.insertMany(products)
+        await Inventory.collection.insertMany(items)
 
-        //check if result from get request contains aforementioned products
+        //check if result from get request contains aforementioned items
         const res = await request.get("/api/inventory").send()
         expect(res.status).toEqual(200);
         expect(res.body.some(entry => entry.name === 'stationerybundle')).toBeTruthy();
-        expect(res.body.some(entry => entry.name === 'productbundle')).toBeTruthy();
+        expect(res.body.some(entry => entry.name === 'itembundle')).toBeTruthy();
     });
 });
 
 describe('GET /:id', () => {
-    it('should return a product if valid id is passed', async () => {
-      //create new product and save it in the database
-      const product = new Inventory({ name: 'stationerybundle',price:45,quantity:33 });
-      await product.save()
+    it('should return a item if valid id is passed', async () => {
+      //create new item and save it in the database
+      const item = new Inventory({ name: 'stationerybundle',price:45,quantity:33 });
+      await item.save()
 
-      const res = await request.get('/api/inventory/' + product._id);
+      const res = await request.get('/api/inventory/' + item._id);
       
       //checking if the id used returns accurate values in the response body
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty('name', product.name);  
-      expect(res.body).toHaveProperty('quantity', product.quantity);  
-      expect(res.body).toHaveProperty('price', product.price);  
+      expect(res.body).toHaveProperty('name', item.name);  
+      expect(res.body).toHaveProperty('quantity', item.quantity);  
+      expect(res.body).toHaveProperty('price', item.price);  
     });
 
     it('should return 404 if no genre with the given id exists', async () => {
@@ -224,3 +224,96 @@ describe('GET /:id', () => {
       expect(res.status).toBe(404);
     });
   });
+
+  describe('PUT /api/inventory/:id', () => {
+    let putName;
+    let putPrice;
+    let putQuantity;
+    let putCategory;
+
+    const exec = async () => {
+      return await request
+        .put('/api/inventory/' + id)
+        .send({ name: putName, price: putPrice, quantity: putQuantity, category: putCategory });
+    }
+
+    beforeEach(async () => {
+      // Before each test we need to create an item and 
+      // put it in the database.      
+      item = new Inventory({ name: 'item1', price:30, quantity:60, category:["food", "perishable"]});
+      await item.save();
+       
+      id = item._id; 
+      putName = "item1.1"
+      putPrice = 40
+      putQuantity = 70
+      putCategory = ["stationery", "non-perish"]
+    })
+
+    it('should update the item if request is valid', async () => {
+        await exec();
+        const updatedItem = await Inventory.findById(item._id);
+        
+        //checking if item details are updated
+        expect(updatedItem.name).toBe(putName);
+        expect(updatedItem.price).toBe(putPrice);
+        expect(updatedItem.quantity).toBe(putQuantity);
+    });
+
+    it('should return the item if request is valid', async () => {
+        const res = await exec();
+        
+        //checking if item details are updated
+        expect(res.body.name).toBe(putName);
+        expect(res.body.price).toBe(putPrice);
+        expect(res.body.quantity).toBe(putQuantity);
+    });
+
+    it('should return 400 if item is less than 5 characters', async () => {
+        putName = '1234'; 
+        const res = await exec();
+  
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual('"name" length must be at least 5 characters long')
+    });
+
+    it('should return 400 if price is not a number', async () => {
+        putPrice = "f55";
+        const res = await exec();
+  
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual('"price" must be a number')
+    });
+
+    it('should return 400 if quantity is not a number', async () => {
+        putQuantity = "f55";
+        const res = await exec();
+  
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual('"quantity" must be a number')
+    });
+
+    it('should return 400 if quantity is not a number', async () => {
+        putName = 1234;
+        const res = await exec();
+  
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual('"name" must be a string')
+    });
+
+    it('should return 400 if quantity is not a number', async () => {
+        putCategory = 23;
+        const res = await exec();
+  
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual('"category" must be an array')
+    });
+
+    it('should return 404 if item with the given id was not found', async () => {
+        id = mongoose_test.Types.ObjectId();
+        const res = await exec();
+  
+        expect(res.status).toBe(404);
+    });
+
+});
